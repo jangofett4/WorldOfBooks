ce4db1391ff1d06497c75311dd681d1b191b3929
Barış Doğan <sulhdogan@gmail.com>
2019-12-25 20:46:59 +0300

    pagebuyhistory fix

--- pageBuyHistory.php
+++ pageBuyHistory.php
@@ -22,33 +22,40 @@
                 <h1 class="display-4">Alışverişe başlamak için <a href="index.php">tıklayın</a>!</h1>
             </div>
         <?php } else { ?>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Ürün</th>
-                        <th scope="col"></th>
-                        <th scope="col">Adet</th>
-                        <th scope="col">Birim Fiyat</th>
-                        <th scope="col">Toplam Fiyat</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    /** @var int $id */
-                    foreach ($cart as $id => $count) {
-                        $key = $con->key("Books", $id);
-                        $book = $con->lookup($key);
-                    ?>
-                        <tr id="bigcart<?php echo $id ?>">
-                            <td class="align-middle"><img src="<?php echo $book["coverpath"] ?>" class="img-bigcart"></td>
-                            <td class="align-middle"> <?php echo $book["name"] ?></td>
-                            <td class="align-middle"><?php echo $count ?></td>
-                            <td class="align-middle"><?php echo $book["cost"] ?> ₺</td>
-                            <td class="align-middle"><?php echo $total ?> ₺</td>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Ürün</th>
+                            <th scope="col"></th>
+                            <th scope="col">Adet</th>
+                            <th scope="col">Birim Fiyat</th>
+                            <th scope="col">Toplam Fiyat</th>
                         </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php
+                        /** @var int $id */
+                        $totalcount = 0;
+                        foreach ($cart as $id => $count) {
+                            $key = $con->key("Books", $id);
+                            $book = $con->lookup($key);
+                            $totalcount += $count;
+                        ?>
+                            <tr id="bigcart<?php echo $id ?>">
+                                <td class="align-middle"><img src="<?php echo $book["coverpath"] ?>" class="img-bigcart"></td>
+                                <td class="align-middle"> <?php echo $book["name"] ?></td>
+                                <td class="align-middle" id="bc<?php echo $id ?>"><?php echo $count ?></td>
+                                <td class="align-middle"><?php echo $book["cost"] ?> ₺</td>
+                                <td class="align-middle"><span id="bt<?php echo $id ?>"><?php echo $book["cost"] * $count ?></span> ₺</td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="row">
+                <h5 class="col align-self-center">Toplam: <b id="cartTotal"><?php echo $total ?></b><b>₺</b></h5>
+            </div>
         <?php } ?>
     </div>
     <?php include "templates/footer.php" ?>
