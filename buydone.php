53966c206037e79dd5557b8ed22e556b258e2744
Barış Doğan <sulhdogan@gmail.com>
2020-01-02 09:43:17 +0300

    Fixed page buy

--- buydone.php
+++ buydone.php
@@ -1,1 +1,39 @@
-Buy complete
+<?php
+require_once "libssn.php";
+?>
+<!doctype html>
+<html lang="tr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>World Of Books</title>
+    <style>
+        .checked {
+            color: orange;
+        }
+    </style>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/wob.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+
+    <script src="js/wobmain.js?"></script>
+</head>
+
+<body>
+    <?php include "templates/nav.php" ?>
+
+    <div class="container container-fixed mt-5">
+        <h1 class="display-4 text-center">Satın alma başarılı, ana sayfaya dönmek için <a href="index.php">tıklayın</a>!</h1>
+    </div>
+    <?php include "templates/footer.php" ?>
+    <!-- Libraries -->
+    <script src="js/jquery-3.4.1.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/fuse.js/3.4.5/fuse.min.js"></script> -->
+</body>
+
+</html>
